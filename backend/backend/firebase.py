c0e8b068e1d4272cfe003f1c65f918f418a92e7d
oatmeal2211 <maxyjared@gmail.com>
2025-04-18 22:18:40 +0800

    updated changes to blockchain and .gitignore file

--- backend/backend/firebase.py
+++ backend/backend/firebase.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("backend/umhack2025-50892-firebase-adminsdk-fbsvc-e9d12f56ba.json")
+cred = credentials.Certificate("backend/umhack2025-50892-firebase-adminsdk-fbsvc-5aa7f72c21.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
