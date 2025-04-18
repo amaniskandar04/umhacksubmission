1db40190512e0ec622b6a6d962a8fe1938bd33f4
amaniskandar04 <alreadypro04@gmail.com>
2025-04-18 22:54:50 +0800

    environment structured

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import CardSlider1 from "./Slider1.js";
+import CardSlider2 from "./Slider2.js";
+import './home.css';
+
+  
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <div>
+        <h2>Ongoing projects</h2>
+          <div>
+            <CardSlider1 />
+          </div>
+      </div>
+      <div>
+        <h2>About us</h2>
+        <p>
+        Easy2Wakaf is a modern, user-friendly platform designed to simplify the way you contribute to waqf. Our mission is to make giving effortless, fully trackable, and completely transparent. With Easy2Waqf, you can make your waqf payments in just a few clicks while easily monitoring where your contributions go and how they’re being used. We believe in building trust through clarity, ensuring every donor stays informed and connected to the causes they support
+        </p>
+        <button>Learn More</button>
+      </div>
+      <div>
+        <h2>Completed Projects</h2>
+        <CardSlider2 />
+      </div>
+    </div>
+    
+  );
+};
+
+export default Home;
