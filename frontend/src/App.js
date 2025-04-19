385405721d757526115664c9f0e73ce868970da1
Kimii <kimimahadzir123@gmail.com>
2025-04-19 22:39:02 +0800

    appjs from aman

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Home from "./components/Home"; // Assuming you will create this component
+import Login from "./components/login";
+import Register from "./components/register";
+import AddProject from "./components/addproject";
+import Projectpage from "./components/projectpage";
+import EditProject from "./components/editproject";
+import Navbar from "./components/Navbar"; // Navbar component for navigation
+
+const AppWrapper = () => {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/register"]; // Add '/register' if you make one
+
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projectpage/:id" element={<Projectpage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/addproject" element={<AddProject />} />
+        <Route path="/editproject" element={<EditProject />} />
+      </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  );
+};
+
+export default App;
