2c8ba5af849e33f386d0d4a926b478694e5f2200
amaniskandar04 <alreadypro04@gmail.com>
2025-04-19 17:58:57 +0800

    login added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,34 @@
 
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home'; // Assuming you will create this component
+import Login from './components/login';
 import Projectpage from './components/projectpage'
 import Navbar from './components/Navbar'; // Navbar component for navigation
 
-const App = () => {
+const AppWrapper = () => {
+  const location = useLocation();
+  const hideNavbarRoutes = ['/login']; // Add '/register' if you make one
+
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
   return (
-    
-    <Router>
-      <Navbar />
+    <>
+      {!shouldHideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/projectpage/:id" element={<Projectpage />} />
-
-       
       </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppWrapper />
     </Router>
   );
 };
